7053910c2e5b2372fca00aed067cffb269b7fd70
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-19 14:56:07 +0530

    :hammer: change port number

--- App.js
+++ App.js
@@ -74,6 +74,6 @@ mongoose
     .catch(err => console.log(err));
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
