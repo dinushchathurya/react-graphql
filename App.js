81021584e6ab757009e1b39280a38a705decd87d
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-19 09:01:40 +0530

    :sparkles: create basic graphql query

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 const express =  require('express');
 const bodyParser = require ('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.listen(3000);
-
 app.use(bodyParser.json());
+
+app.use('/api/v1', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () =>{
+            return ['Romantic', 'Coding']
+        },
+        createEvent: (args) => {
+            const eventName =args.name;
+            return eventName;
+        }
+    } 
+}));
+
+app.listen(3000);
