6bff9fae4c27c8853dddca2587ea972cc97d6319
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-21 11:54:35 +0530

    :hammer: replace date with dateToString helper

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcryptjs');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
+const { dateToString } = require('../../helpers/date');
 
 const transformEvent = event => {
     return {
         ...event._doc,
         _id: event.id,
-        date: new Date(event._doc.date).toISOString(),
+        date: dateToString(event._doc.date),
         creator: user.bind(this, event.creator) 
     }
 }
@@ -63,8 +64,8 @@ module.exports = {
                     _id:booking.id, 
                     user: user.bind(this, booking._doc.user),
                     event: singleEvent.bind(this, booking._doc.event),
-                    createdAt: new Date(booking._doc.createdAt).toISOString(),
-                    updatedAt: new Date(booking._doc.updatedAt).toISOString()
+                    createdAt: dateToString(booking._doc.createdAt),
+                    updatedAt: dateToString(booking._doc.updatedAt)
                 }
             })
         } catch (err) {
@@ -127,8 +128,8 @@ module.exports = {
             _id: result.id,
             user: user.bind(this, booking._doc.user),
             event: singleEvent.bind(this, booking._doc.event),
-            createdAt: new Date(result._doc.createdAt).toISOString(),
-            updatedAt: new Date(result._doc.updatedAt).toISOString()
+            createdAt: dateToString(result._doc.createdAt),
+            updatedAt: dateToString(result._doc.updatedAt)
         }
     },
     cancelBooking : async args => {
