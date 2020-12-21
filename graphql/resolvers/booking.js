f42db58e3e064849bf1bb1f30c5af17f9acffcdb
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-21 17:00:02 +0530

    :hammer: protect functions with auth middleware

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -4,7 +4,10 @@ const { transformEvent, transformBooking } = require('../resolvers/merge');
 
 module.exports = {
     /* fetch booking */
-    booking: async () => {
+    booking: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated');
+        }
         try {
             const bookings = await Booking.find();
             return bookings.map(booking => {
@@ -15,17 +18,23 @@ module.exports = {
         }
     },
     /* create booking */
-    bookEvent: async args => {
+    bookEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated');
+        }
         const fetchedEvent = await Event.findOne({ _id: args.eventId })
         const booking = new Booking({
-            user: '5fdee45b4f8d04608cc780fe',
+            user: req.userId,
             event: fetchedEvent
         });
         const result = await booking.save();
         return transformBooking(result);
     },
     /* cancel booking */
-    cancelBooking: async args => {
+    cancelBooking: async (args,req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated');
+        }
         try {
             const booking = await Booking.findById(args.bookingId).populate('event');
             const event = transformEvent(booking.event);
