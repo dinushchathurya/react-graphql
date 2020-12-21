234d6bea6455af9ff889c858e0cba7a0cf7dcb8c
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-21 22:18:07 +0530

    :sparkles: create route paths for auth,events & booking components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import MainNav from './components/Navigation/MainNav';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import Auth from './pages/Auth';
+import Events from './pages/Events';
+import Bookings from './pages/Bookings';
+
+class App extends Component {
+  render() { 
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNav />
+          <main className="main-content">
+              <Switch>
+                <Redirect from="/" to="/auth" component={Auth} exact />
+                <Route path="/auth" component={Auth} />
+                <Route path="/events" component={Events} />
+                <Route path="/bookings" component={Bookings} />
+              </Switch>
+            </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
