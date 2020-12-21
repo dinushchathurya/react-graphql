8f6991b88161b6f438dfd512800aa6300309de2f
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-21 11:00:23 +0530

    :sparkles: create cancel booking functionality

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -21,6 +21,18 @@ const events = async eventIds => {
     }
 }
 
+const singleEvent = async eventId => {
+    try {
+        const event = await Event.findById(eventId);
+        return { 
+            ...event._doc, 
+            _id: event.id, 
+            creator: user.bind(this, event.creator) }
+    } catch (err) {
+        throw err;
+    }
+}
+
 const user = async userId => {
     try {
     const user = await User.findById(userId)
@@ -54,6 +66,8 @@ module.exports = {
                 return { 
                     ...booking._doc, 
                     _id:booking.id, 
+                    user: user.bind(this, booking._doc.user),
+                    event: singleEvent.bind(this, booking._doc.event),
                     createdAt: new Date(booking._doc.createdAt).toISOString(),
                     updatedAt: new Date(booking._doc.updatedAt).toISOString()
                 }
@@ -121,8 +135,24 @@ module.exports = {
         return {
             ...result._doc,
             _id: result.id,
+            user: user.bind(this, booking._doc.user),
+            event: singleEvent.bind(this, booking._doc.event),
             createdAt: new Date(result._doc.createdAt).toISOString(),
             updatedAt: new Date(result._doc.updatedAt).toISOString()
+        }
+    },
+    cancelBooking : async args => {
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event');
+            const event = {
+                ...booking.event._doc, 
+                _id: booking.event.id,
+                creator: user.bind(this, booking.event._doc.creator)
+            };
+            await Booking.deleteOne({_id: args.bookingId});
+            return event;
+        } catch (err) {
+            throw err;
         }
     }
 }
