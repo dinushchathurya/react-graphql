6742a48fcf3368a820617550c112fda80e043df3
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-21 22:17:01 +0530

    :sparkles: create booking page component

---
+++ client/src/pages/Bookings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Bookings extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Bookings Work</h1>
+            </div>
+        )
+    }
+}
+
+export default  Bookings;
