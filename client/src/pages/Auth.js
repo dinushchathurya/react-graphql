4328ead6a6ce6f2667d0bb88e2c66b7a97b6115c
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-22 10:36:20 +0530

    :sparkles: create auth component

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import './Auth.css';
 
 class Auth extends Component {
     render() {
         return (
-            <div>
-                <h1>Auth works</h1>
-            </div>
+            <form className="auth-form">
+                <div className="form-control">
+                    <label for= "email">Email</label>
+                    <input type="email" id="email" />
+                </div>
+                <div className="form-control">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <div className="form-actions">
+                    <button type="button">Switch to Signup</button>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         )
     }
 }
