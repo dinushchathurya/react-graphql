27e38492fb9ea0b20c08a732fb166d31b9a39727
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-22 13:27:43 +0530

    :wheelchair: allow CORS from different origins

--- App.js
+++ App.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Method', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
+
 app.use(isAuth);
 
 app.use('/api/v1', graphqlHTTP({
