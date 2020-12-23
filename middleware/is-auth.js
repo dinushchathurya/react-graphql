f360c4879b45d2ac9f9d48982edc8eff75760325
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-23 22:27:54 +0530

    :bug: fix typo in  isAuth request

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
-        req.isAuth - false;
+        req.isAuth = false;
         return next();
     }
     const token = authHeader.split(' ')[1];
