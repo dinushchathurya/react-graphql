893c99ec6c78cad11e6f7367e694db39c35f827f
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-24 18:53:47 +0530

    :hammer: Configure event view button functionality

--- client/src/components/Events/EventList/EventItem/EventItem.js
+++ client/src/components/Events/EventList/EventItem/EventItem.js
@@ -5,14 +5,14 @@ import './EventItem.css';
 const EventItem = props => (
     <li key={props.eventId} className="events__list-item">
         <div>
-            <h1>{props.title}</h1>
-            <h2>$19.99</h2>
+            <h2>{props.title}</h2>
+            <h3>${props.price} - {new Date(props.date).toLocaleDateString()}</h3>
         </div>
         <div>
             {props.userId === props.creatorId ? (
                 <p>You are the owner of this event </p>
                 ) : (
-                <button className="btn">View Event</button>
+                    <button className="btn" onClick={props.onDetail.bind(this, props.eventId)}>View Event</button>
             )}
         </div>
     </li>
